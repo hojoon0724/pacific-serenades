71977c67c1bff51d253282369d136c83775d3503
hojoon0724 <hojoon0724@gmail.com>
2024-10-31 15:29:54 -0700

    removed composer section from landing

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   return (
     <div className="top-container flex flex-col justify-center items-center">
       <Landing />
-      <ComposerSection composer={albanBerg} />
+      {/* <ComposerSection composer={albanBerg} /> */}
     </div>
   );
 }
