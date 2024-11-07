452eadcd3bf4c488f13df4fbd6d7c856240029f4
hojoon0724 <hojoon0724@gmail.com>
2024-11-07 14:17:03 -0800

    script to merge musician json files complete

---
+++ workshop/makeMusiciansFromList.js
@@ -0,0 +1,49 @@
+const fs = require("fs").promises;
+const path = require("path");
+
+const sourceFile = "./allMusicians.json";
+
+function writeToFile(file, content) {
+  fs.writeFile(file, JSON.stringify(content, null, 2));
+}
+
+async function readFullList(filePath) {
+  let result = [];
+  try {
+    const content = await fs.readFile(filePath, "utf-8");
+    // console.log(content); // good
+    const parsedData = Object.entries(JSON.parse(content));
+    parsedData.forEach((item) => {
+      console.log(item[0]);
+      let musician = {
+        [item[0]]: {
+          ...item[1],
+        },
+      };
+      result = { ...result, ...musician };
+    });
+  } catch (err) {
+    console.log("error while reading file", err);
+  }
+  console.log(result);
+  return result;
+}
+
+async function makeIndividualMusician() {
+  let dataToBeWritten = await readFullList(sourceFile);
+  return dataToBeWritten;
+}
+
+// makeIndividualMusician();
+
+makeIndividualMusician().then((data) => {
+  for (const [key, value] of Object.entries(data)) {
+    const object = {
+      [key]: value,
+    };
+    // console.log(value);
+    writeToFile(`./processedMusicians/${key}.json`, object);
+  }
+});
+// console.log(`entry ${index}`, entry);
+// console.log(entry[0]);
