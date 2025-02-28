d44ff808b49be74c981958611fd70753dc635a0c
Hojoon Kim <hojoon0724@gmail.com>
2025-02-28 14:51:40 -0800

    fixed keys

--- src/pages/library/past-seasons/index.jsx
+++ src/pages/library/past-seasons/index.jsx
@@ -8,51 +8,56 @@ import CapitalizeTitle from "@/components/CapitalizeTitle";
 export default function PastSeasons() {
   return (
     <div className="flex flex-col items-center mt-8">
-    <h1>Past Seasons</h1>
-    <div className="all-seasons-container grid grid-cols-1 gap-8 m-2 mt-8 sm:m-8">
-      {Object.entries(seasonData).map(([season, concerts]) => {
-        return (
-          <div className="season" key={season.slice(0, 4)}>
-            <div className="season-container bg-blue-50 p-4 rounded-xl">
-              <div className="season-name text-xl font-bold bg-blue-700 rounded-lg px-4 py-2 text-blue-50">
-                {season}
-              </div>
-              <div className="concerts-container">
-                {concerts.map((concertId) => {
-                  return (
-                    <div
-                      className="concert-container bg-teal-100 px-4 py-2 mt-4 rounded-lg border-1 border-blue-300"
-                      key={concertId}
-                    >
-                      <h5 className="concert-title text-blue-800 pb-4" key={concertsData[concertId].id}>
-                        <CapitalizeTitle str={concertsData[concertId].concertTitle} />
-                      </h5>
+      <h1>Past Seasons</h1>
+      <div className="all-seasons-container grid grid-cols-1 gap-8 m-2 mt-8 sm:m-8">
+        {Object.entries(seasonData).map(([season, concerts]) => {
+          return (
+            <div className="season" key={season.slice(0, 4)}>
+              <div className="season-container bg-blue-50 p-4 rounded-xl">
+                <div className="season-name text-xl font-bold bg-blue-700 rounded-lg px-4 py-2 text-blue-50">
+                  {season}
+                </div>
+                <div className="concerts-container">
+                  {concerts.map((concertId) => {
+                    return (
                       <div
-                        className="concert-program-work-container grid items-center gap-0 sm:gap-2 sm:grid-cols-[min-content_min-content_auto]"
+                        className="concert-container bg-teal-100 px-4 py-2 mt-4 rounded-lg border-1 border-blue-300"
+                        key={concertId}
                       >
-                        {concertsData[concertId].program.map((work, index) => {
-                          return (
-                            // <div className="concert-program-work flex w-full" key={index}>
-                            <>
-                              <div className="concert-program-work-composer pt-4 sm:w-max sm:pt-0">
-                                {composersData[worksData[work].workComposer].fullName}
-                              </div>
-                              <div className="hidden sm:flex divider w-1 px-2">|</div>
-                              <div className="concert-program-work-title w-full pt-0">{worksData[work].workName}</div>
-                            </>
-                            // </div>
-                          );
-                        })}
+                        <h5 className="concert-title text-blue-800 pb-4" key={concertsData[concertId].id}>
+                          <CapitalizeTitle str={concertsData[concertId].concertTitle} />
+                        </h5>
+                        <div className="concert-program-work-container grid items-center gap-0 sm:gap-2 sm:grid-cols-[min-content_min-content_auto]">
+                          {concertsData[concertId].program.map((work, index) => {
+                            return (
+                              // <div className="concert-program-work flex w-full" key={index}>
+                              <>
+                                <div
+                                  className="concert-program-work-composer pt-4 sm:w-max sm:pt-0"
+                                  key={`composerDiv${work}`}
+                                >
+                                  {composersData[worksData[work].workComposer].fullName}
+                                </div>
+                                <div className="hidden sm:flex divider w-1 px-2" key={`separatorDiv${work}`}>
+                                  |
+                                </div>
+                                <div className="concert-program-work-title w-full pt-0" key={`titleDiv${work}`}>
+                                  {worksData[work].workName}
+                                </div>
+                              </>
+                              // </div>
+                            );
+                          })}
+                        </div>
                       </div>
-                    </div>
-                  );
-                })}
+                    );
+                  })}
+                </div>
               </div>
             </div>
-          </div>
-        );
-      })}
-    </div>
+          );
+        })}
+      </div>
     </div>
   );
 }
