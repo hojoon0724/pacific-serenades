c619a9a3767e6f35b39304a71ef831aa0c15b1ca
Hojoon Kim <hojoon0724@gmail.com>
2025-02-28 12:46:38 -0800

    fixed composers broken photo component

--- src/components/ComposerPage/ComposerCard.jsx
+++ src/components/ComposerPage/ComposerCard.jsx
@@ -1,17 +1,21 @@
-import { Card, CardFooter, Image, Button } from "@nextui-org/react";
+import { Card, CardFooter, Button } from "@nextui-org/react";
+import Image from "next/image";
 
 export default function ComposerCard({ composer }) {
   return (
     <Card isFooterBlurred radius="lg" className="border-none aspect-square">
       {composer.photo === "" ? (
-        <Image src="/icons/person.svg" className="object-cover" alt="person icon" width={200} />
+        <Image src="/icons/person.svg" className="object-cover" alt="person icon" width={200} height={200} />
       ) : (
         <Image
-          alt={`${composer.fullName} photo`}
-          className="object-cover"
-          height={200}
           src={composer.photo}
-          width={200}
+          alt={`${composer.fullName} photo`}
+          objectFit="cover"
+          objectPosition="center"
+          layout="fill"
+          sizes="200px"
+          // height={200}
+          // width={200}
         />
       )}
       <CardFooter className="justify-center bg-white/50 border-white/20 border-1 overflow-hidden py-1 absolute backdrop-blur-lg backdrop-brightness-200 before:rounded-xl rounded-large bottom-1 w-[calc(100%_-_8px)] shadow-small ml-1 z-10">
